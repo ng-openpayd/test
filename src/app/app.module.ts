f7286439535f075cf09869bcea19b96e0d6b9621
V555-pro <website_dvp@outlook.com>
2020-02-20 14:13:53 +0100

    added slick carousel and implemented details-wireframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { StoreModule } from "@ngrx/store";
 import { EffectsModule } from "@ngrx/effects";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { DifferenceInDaysPipe } from './difference-in-days.pipe';
+import { NgxSlickJsModule } from "ngx-slickjs";
 
 @NgModule({
   declarations: [
@@ -33,7 +34,18 @@ import { DifferenceInDaysPipe } from './difference-in-days.pipe';
     MatCardModule,
     StoreModule.forRoot(launchReducers),
     EffectsModule.forRoot(launchEffects),
-    StoreDevtoolsModule.instrument()
+    StoreDevtoolsModule.instrument(),
+    NgxSlickJsModule.forRoot({
+      links: {
+        jquery: "https://code.jquery.com/jquery-3.4.0.min.js",
+        slickJs:
+          "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js",
+        slickCss:
+          "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css",
+        slickThemeCss:
+        "https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/launch-details/launch-details.component.ts
+++ src/app/launch-details/launch-details.component.ts
@@ -2,6 +2,7 @@ import { Component, ChangeDetectionStrategy } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { map, switchMap } from "rxjs/operators";
 import { LaunchDetailsGQL } from "../services/spacexGraphql.service";
+import { Slick } from 'ngx-slickjs';
 
 @Component({
   selector: "app-launch-details",
@@ -10,9 +11,10 @@ import { LaunchDetailsGQL } from "../services/spacexGraphql.service";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LaunchDetailsComponent {
+  current_image = 0;
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly launchDetailsService: LaunchDetailsGQL
+    private readonly launchDetailsService: LaunchDetailsGQL,
   ) {}
 
   launchDetails$ = this.route.paramMap.pipe(
@@ -20,4 +22,12 @@ export class LaunchDetailsComponent {
     switchMap(id => this.launchDetailsService.fetch({ id })),
     map(res => res.data.launch)
   );
+
+  config: Slick.Config = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    prevArrow: '<i class="material-icons" style="position:absolute;top:36%;left:-10px;cursor:pointer;">arrow_back_ios</i>',
+    nextArrow: '<i class="material-icons" style="position:absolute;top:36%;right:-18px;cursor:pointer;">arrow_forward_ios</i>'
+  }
 }
